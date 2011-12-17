6ca701074b59df5842ca7cdfc279102fdb6aeb98
Ji Han <hanji1984@gmail.com>
2011-12-17 19:51:32 +0800

    revision 1

--- allocator.hpp
+++ allocator.hpp
@@ -1,15 +1,20 @@
 #ifndef _ALLOCATOR_INCLUDED_
 #define _ALLOCATOR_INCLUDED_
 
-/**
- * naive allocator, a wrapper around malloc/free
- * (c) 2011 Ji Han
- */
-
 #include <cstdio>
 #include <cstdlib>
 #include <exception>
 #include <limits>
+
+#if defined(_DEBUG)
+#define TRACEVAL(v) fprintf(stderr, "[%llx]", (unsigned long long)(v))
+#define TRACECSTR(s) fprintf(stderr, "{%s}", s)
+#define ENDL() fprintf(stderr, "\n")
+#else
+#define TRACEVAL(v)
+#define TRACECSTR(s)
+#define ENDL()
+#endif
 
 namespace test {
 
@@ -64,12 +69,12 @@ template<> class allocator<void>
 
 // naive allocator (simply a wrapper around malloc/free)
 // ctor does nothing
-template<typename T> allocator<T>::allocator() throw() {}
-template<typename T> allocator<T>::allocator(const allocator<T> &) throw() {}
-template<typename T> template<typename U> allocator<T>::allocator(const allocator<U> &) throw() {}
+template<typename T> allocator<T>::allocator() throw() {TRACECSTR(__FUNCTION__); ENDL();}
+template<typename T> allocator<T>::allocator(const allocator<T> &) throw() {TRACECSTR(__FUNCTION__); ENDL();}
+template<typename T> template<typename U> allocator<T>::allocator(const allocator<U> &) throw() {TRACECSTR(__FUNCTION__); ENDL();}
 
 // dtor has nothing to do
-template<typename T> allocator<T>::~allocator() throw() {}
+template<typename T> allocator<T>::~allocator() throw() {TRACECSTR(__FUNCTION__); ENDL();}
 
 // c.f. ISO/IEC 14882 requirements for operator==()
 template<typename T>
@@ -108,9 +113,7 @@ allocator<T>::allocate(typename allocator<T>::size_type n,
                        typename allocator<T>::const_pointer u)
 throw(std::bad_alloc)
 {
-#if defined(_DEBUG)
-    fprintf(stderr, "%s (%d, %x)\n", __FUNCTION__, n, u);
-#endif
+    TRACECSTR(__FUNCTION__); TRACEVAL(n); TRACEVAL(u); ENDL();
     typename allocator<T>::pointer p = nullptr;
     p = static_cast<typename allocator<T>::pointer>(malloc(static_cast<std::size_t>(sizeof(T) * n)));
     if (!p) { throw(std::bad_alloc()); }
@@ -123,9 +126,7 @@ allocator<T>::deallocate(typename allocator<T>::pointer p,
                          typename allocator<T>::size_type n)
 throw()
 {
-#if defined(_DEBUG)
-    fprintf(stderr, "%s (%x, %d)\n", __FUNCTION__, p, n);
-#endif
+    TRACECSTR(__FUNCTION__); TRACEVAL(p); TRACEVAL(n); ENDL();
     free(p);
 }
 
@@ -135,9 +136,7 @@ allocator<T>::construct(typename allocator<T>::pointer p,
                         typename allocator<T>::const_reference val)
 throw()
 {
-#if defined(_DEBUG)
-    fprintf(stderr, "%s (%x, %d)\n", __FUNCTION__, p, val);
-#endif
+    TRACECSTR(__FUNCTION__); TRACEVAL(p); TRACEVAL(val); ENDL();
     ::new (static_cast<void *>(p)) T(val);
 }
 
@@ -145,9 +144,7 @@ template<typename T>
 void
 allocator<T>::destroy(typename allocator<T>::pointer p) throw()
 {
-#if defined(_DEBUG)
-    fprintf(stderr, "%s (%x)\n", __FUNCTION__, p);
-#endif
+    TRACECSTR(__FUNCTION__); TRACEVAL(p); ENDL();
     p->~T();
 }
 
